f8751082aaf680ccdf2dd46f4d59fc8d9b00cc38
Kseniya Borisova <68520114+kseniabrsv@users.noreply.github.com>
2021-04-21 19:20:18 +0700

    Add files via upload

---
+++ ExistingEventApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ExistingEventApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public int Balance { get; set; }
+
+        public List<ExistingEvent> EventsWithRates { get; set; }
+    }
+}
