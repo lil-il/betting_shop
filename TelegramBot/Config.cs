02862b6dfbdc0f4acd5df150719da162848bdbe1
iprok <i.prokofev@g.nsu.ru>
2021-05-15 14:04:02 +0700

    added Token to json

--- TelegramBot/Config.cs
+++ TelegramBot/Config.cs
@@ -2,6 +2,6 @@
 {
     public class Config
     {
-        public static string Token = "1669291330:AAHZ-qdBkVbGS7TaOJm85XLyDMxqG3C65bQ";
+        public static string Token;
     }
 }
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.IO;
 using BettingShop.TelegramBot.Message;
+using Newtonsoft.Json;
 using Telegram.Bot;
 
 namespace BettingShop.TelegramBot
 {
     internal static class Program
-        {
-            private static ITelegramBotClient botClient;
-            private static BotHandler telegramHandler;
-            private static ICommandTypeParser _typeParser;
-            private static UserMessageParser parser;
+    {
+        private static ITelegramBotClient botClient;
+        private static BotHandler telegramHandler;
+        private static ICommandTypeParser _typeParser;
+        private static UserMessageParser parser;
 
-            static void Main()
+        static void Main()
         {
+            var r = new StreamReader("C:/Users/iprok/bot/betting_shop/TelegramBot/Config.json");
+            var json = r.ReadToEnd();
+            var config = JsonConvert.DeserializeObject<Config>(json);
             var token = Config.Token;
 
             botClient = new TelegramBotClient(token);
